cf8765ce64a95ac51adbfdd47d615bc705bf4d4d
Khalid Khalil <coderizer@aol.com>
2021-01-04 17:47:20 +0200

    Add event listeners to off/on badges

--- public/js/app.js
+++ public/js/app.js
@@ -5,10 +5,15 @@
 */
 
 // Select DOM elements
-const turnOnButton = document.querySelector("#turn-on");
-const turnOffButton = document.querySelector("#turn-off");
-const onStatus = document.querySelector(".on");
-const offStatus = document.querySelector(".off");
+
+// Light 1
+const turnOnButton1 = document.querySelector("#turn-on1");
+const turnOffButton1 = document.querySelector("#turn-off1");
+
+// Light 2
+const turnOnButton2 = document.querySelector("#turn-on2");
+const turnOffButton2 = document.querySelector("#turn-off2");
+
 const controls = document.querySelector(".controls");
 const spinner = document.querySelector(".spinner");
 
@@ -43,28 +48,119 @@ function onConnectionLost(responseObject) {
 
 // called when a message arrives
 function onMessageArrived(message) {
+  // testing and debugging
   console.log(
     "onMessageArrived:",
     message.destinationName,
     message.payloadString
   );
+  /*
+  
+    Respond to different events emitted by Arduino
+
+  */
+
+  // Light 01 status
+  if (message.destinationName === "/LIGHT/01/STATUS") {
+    if (message.payloadString === "ON") {
+      // Turn on
+      turnOnLight(1);
+    } else {
+      // Turn off
+      turnOffLight(1);
+    }
+  }
+
+  // Light 02 status
+  if (message.destinationName === "/LIGHT/02/STATUS") {
+    if (message.payloadString === "ON") {
+      // Turn on
+      turnOnLight(2);
+    } else {
+      // Turn off
+      turnOffLight(2);
+    }
+  }
 }
 
-// Buttons event handlers
-turnOffButton.addEventListener("click", function () {
+/*
+ *
+ * Utility functions
+ *
+ */
+
+function turnOnLight(number) {
+  const offBadge = document.querySelector(`off${number}`);
+  const onBadge = document.querySelector(`on${number}`);
+  // Hide Off badge
+  offBadge.style.display = "none";
+  // Show on badge
+  onBadge.style.display = "block";
+}
+
+function turnOffLight(number, state) {
+  const offBadge = document.querySelector(`off${number}`);
+  const onBadge = document.querySelector(`on${number}`);
+  // Hide On badge
+  onBadge.style.display = "none";
+  // Show Off badge
+  offBadge.style.display = "block";
+}
+
+/*
+ *
+ * Buttons Event Handlers
+ *
+ */
+
+/*
+
+  Light 1
+
+*/
+
+// Turn off
+turnOffButton1.addEventListener("click", function () {
   // Hide Turn Off button and show Turn On
-  turnOffButton.style.display = "none";
-  turnOnButton.style.display = "block";
+  turnOffButton1.style.display = "none";
+  turnOnButton1.style.display = "block";
   message = new Paho.Message("OFF");
-  message.destinationName = "/LED/CONTROL";
+  message.destinationName = "/LIGHT/01/CONTROL";
   client.send(message);
 });
 
-turnOnButton.addEventListener("click", function () {
+// Turn On
+turnOnButton1.addEventListener("click", function () {
   // Hide Turn On button and show Turn Off
-  turnOnButton.style.display = "none";
-  turnOffButton.style.display = "block";
+  turnOnButton1.style.display = "none";
+  turnOffButton1.style.display = "block";
   message = new Paho.Message("ON");
-  message.destinationName = "/LED/CONTROL";
+  message.destinationName = "/LIGHT/01/CONTROL";
+  client.send(message);
+});
+
+/*
+
+  Light 2
+
+*/
+
+// Turn Off
+turnOffButton2.addEventListener("click", function () {
+  // Hide Turn Off button and show Turn On
+  turnOffButton2.style.display = "none";
+  turnOnButton2.style.display = "block";
+  message = new Paho.Message("OFF");
+  message.destinationName = "/LIGHT/02/CONTROL";
+  client.send(message);
+});
+
+// Turn On
+turnOnButton2.addEventListener("click", function () {
+  // Hide Turn On button and show Turn Off
+  turnOnButton2.style.display = "none";
+  turnOffButton2.style.display = "block";
+  message = new Paho.Message("ON");
+  message.destinationName = "/LIGHT/02/CONTROL";
   client.send(message);
 });
